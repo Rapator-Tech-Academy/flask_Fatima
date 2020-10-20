9ed452aad5f373a808e18678c031ad6d321b92be
Askerova Fatima <a.fatima1297@gmail.com>
2020-10-20 19:03:23 +0400

    wtforms

--- tech_blog.py
+++ tech_blog.py
@@ -1,5 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd37836b284dedee931e81f275733bd9d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db =  SQLAlchemy(app)
+
+class User(db.Model):
+    pass
 
 posts = [
     {
@@ -8,7 +17,6 @@ posts = [
         'content' : 'First post content',
         'date_posted' : 'October 16, 2020'   
     },
-
     {
         'author' : 'Jane Doe',
         'title' : 'Blog Post 2',
@@ -26,6 +34,24 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/database')
+def database():
+    return render_template('about.html', title='Database')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for{form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
